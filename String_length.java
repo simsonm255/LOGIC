ab099ea55565e01b1305bd0c86c33bec2d1f6538
sims91502 <85046038+sims91502@users.noreply.github.com>
2022-08-07 20:06:07 +0530

    Create String_length.java
    Program accept a string and prints the length of the string.

---
+++ String_length.java
@@ -0,0 +1,11 @@
+public class Main{
+      public static void main(String args[]){
+          Scanner sc_input = new Scanner(System.in); // sc_input is a object name for getting input
+          
+          String str = sc_input.next(); // To get input string and store string value in 'str'
+          
+          int len = str.length(); // length() is a method, which returns length of a string
+          
+          System.out.println(len); // print length of the string
+      }
+}
